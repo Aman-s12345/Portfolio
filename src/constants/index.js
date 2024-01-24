fe461a3420c93728b47a0f0ba4db86a895df19f8
unknown <amansinghbiuri@gmail.com>
2024-01-25 02:42:31 +0530

    commit

---
+++ src/constants/index.js
@@ -0,0 +1,232 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Full stack Developer",
+        company_name: "MERN",
+        icon: github,
+        link: "https://github.com/Aman-s12345/",
+        iconBg: "#accbe1",
+        date: "March 2021 - Present",
+        points: [
+            "Developing and maintaining web applications using MERN and other related technologies.",
+            "Collaborating with cross-functional teams including ML developer, Devops developer, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other teammate.",
+        ],
+    },
+    {
+        title: "Problem Solving Skills",
+        company_name: "GFG",
+        icon: "./gfg.png",
+        link : "https://auth.geeksforgeeks.org/user/its__aman10",
+        iconBg: "#fff",
+        date: "June 2021 - Present",
+        points: [
+            "Mastering problem-solving through the completion of 400+ Data Structure and Algorithm (DSA) questions on GeeksforGeeks and LeetCode.",
+            "Actively participated in 25+ online coding contests on platforms like GFG, CodeChef, and CodeForces.",
+            "Achieving a global rank below 200 in Starter 48 in codechef.",
+        ],
+    },
+    {
+        title: "Coding and Other tools",
+        company_name: "C++ | Java | AI-tools",
+        icon: "./java_.png",
+        iconBg: "#b7e4c7",
+        date: "Jan 2021 - Present",
+        points: [
+            "Proficient in advanced-level programming languages including C, C++, Java, and Python, with expertise in object-oriented programming across all three.",
+            "Certified AI-tool user in ChatGPT, Black Box AI, and other technologies.",
+            "Certified in Microsoft tools such as Excel, Word, and PowerPoint, showcasing a diverse skill set in both programming and essential productivity tools.",
+            
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Meta",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "Jan 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Aman-s12345/',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/aman-singh-29055223b/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Study Notion',
+        description: 'StudyNotion is a fully functional ed-tech platform that enables users to create, consume, and rate educational content.',
+        link: 'https://github.com/Aman-s12345/study-notion',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'PaySwift',
+        description: 'PaySwift is a cutting-edge payment gateway solution that mirrors the seamless experience of Razorpay.',
+        link: 'https://github.com/Aman-s12345/razorpay-clone',
+    }
+    ,
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Shoping Cart',
+        description: 'Created a frontend replica of the popular shoping platform "Flipcart," enabling users to shop and add to cart.',
+        link: 'https://github.com/Aman-s12345/shoping_cart',
+    },
+   ,
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-pink',
+        name: 'Whether App',
+        description: 'A weather application developed using JavaScript, offering detailed insights into weather conditions,temperature, humidity, rain predictions, and wind speed.',
+        link: 'https://github.com/Aman-s12345/weatherapp',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Hostal Buddy',
+        description: 'Hostel Buddy is a platform designed to facilitate a sharing economy within a hostel community, enabling hostelers to easily share items and resources among themselves.',
+        link: 'https://github.com/Aman-s12345/Hostel-boddy',
+    },
+    {
+        iconUrl:  snapgram,
+        theme: 'btn-back-yellow',
+        name: 'Blogshala (blog-web)',
+        description: 'App that leverages new technology to users generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/Aman-s12345/blog-website',
+    }
+];
